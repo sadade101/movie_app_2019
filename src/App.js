87e6e6d26ee3a6ecb09eacff2b3b8487a5cc552a
robin <robin@cubeai.ai>
2020-07-01 15:55:04 +0900

    컴포넌트 유니크 키 워닝 문제 해결

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
-function Food({ fav }) {
-  console.log(fav)
-  return <h1>I Like {fav}</h1>
+function Food(prop) {
+  console.log(prop)
+  return <h1 key={prop.fav}>I Like {prop.fav}</h1>
 }
 
-const foodILike = ["라면", "쭈꾸미", "고기"]
+const foodILike = [
+  {
+    id : 1,
+    name : "라면"
+  },
+  {
+    id : 2,
+    name : "쭈꾸미"
+  },
+  {
+    id : 3,
+    name : "고기"
+  }
+]
+
+function renderFood(dish) {
+  console.log(dish)
+  return <Food key={dish.id} fav={dish.name} />
+}
+
+
 
 function App() {
   return (
-    <div>
-      {foodILike.map(food => {
-        return <Food fav={food}/> 
-      })};
+    <div> 
+      {foodILike.map(renderFood)}
     </div>
   );
 }
