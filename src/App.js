3e3d7edc8aba2bc146be2ea72a6ff47cd3e414c8
robin <robin@cubeai.ai>
2020-07-01 14:59:23 +0900

    propery를 이용한 파라미터 전달

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Potato from './Potato'
+
+function Food({ fav }) {
+  console.log(fav)
+  return <h1>I Like {fav}</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="라면" /> 
+      <Food fav="쭈꾸미" /> 
+      <Food fav="고기" /> 
+      <Food fav="김치찌개" /> 
     </div>
   );
 }
