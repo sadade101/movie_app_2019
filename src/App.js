a07b188419fb316e27e13dfd55fc0dc4dec45b7a
robin <robin@cubeai.ai>
2020-07-01 17:10:23 +0900

    숫자계산 람다식으로 변경

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types'
 
 class App extends React.Component {
+  state = {
+    count: 0
+  }
+  add = () => {
+    console.log("add")
+    
+    this.setState(current => ({count:current.count+1}))
+  }
+  minus = () => {
+    console.log("minus")
+    this.setState(current => ({count:current.count-1}))
+  }
   render() {
-    return <h1>I am class component</h1>
+    return (
+      <div>
+        <h1>the number is : {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
   }
 }
 
