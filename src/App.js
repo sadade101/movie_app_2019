cfa8c4087ca625c0eeaea6146bd2abf2a94c8c8e
robin <robin@cubeai.ai>
2020-07-02 18:09:01 +0900

    네비게이션 메뉴 css 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import About from './router/About'
 import Home from './router/Home'
-import { HashRouter, Router, Route } from "react-router-dom"
+import Navigation from './component/Navigation'
+import { HashRouter, Router, Route, BrowserRouter } from "react-router-dom"
    
 
 function App() {
-  return <HashRouter>
-    <Route path="/" exact={true} component={Home}>
-    </Route>
-    <Route path="/about" component={About}>
-    </Route>
-  </HashRouter>
-};
+  return (
+    <HashRouter>
+      <Navigation/>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
+}
 
 export default App;
--- src/component/Movies.js
+++ src/component/Movies.js
@@ -8,13 +8,13 @@ function Movie({id, title, summary, poster, year, genres}) {
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className ="movie__title">{title}</h3>
-                <h5 className ="movie__year">{year}</h5>
-                <p className ="movie__summary">{summary}</p>
-                <ul className ="movie_genre">
+                <h5 className ="movie__year">{year}</h5>               
+                <ul className ="movie__genres">
                     {
                         genres.map((genre,index) => <li key={index}> {genre} </li>)
                     }
                 </ul>
+                <p className ="movie__summary">{summary.slice(0,180)}...</p>
             </div>
         </div>
 
