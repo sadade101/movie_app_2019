1c30e326cb094c1aa611c384153a6fa417bb78bf
robin <robin@cubeai.ai>
2020-07-02 16:32:49 +0900

    Router를 위한 준비 과정

--- src/App.js
+++ src/App.js
@@ -1,68 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movies';
-import './App.css'
-//import PropTypes from 'prop-types'
 
-class App extends React.Component {
-
-  state = {
-    isLoading: true,
-    movies : []
-  };
-
-  getMovies = async () => {
-    const result = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    const {
-      data: {
-        data : {
-            movies 
-        }
-      } 
-    } = result;
-    //console.log(result)
-    //console.log(movies)
-    return movies
-  };
-
-  componentDidMount() {
-    this.getMovies().then(result => {
-      //console.log(result);
-      this.setState({isLoading:false, movies:result}) 
-    }); 
-  }
-
-  componentDidUpdate() {
-  //console.log(this.state.movies)
-  }
-
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section>
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
-}
+function App() {
+  return <span>Somthing</span>
+};
 
 export default App;
