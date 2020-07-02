e3bd84212d7aa56b96c6743e62e98e9675071729
robin <robin@cubeai.ai>
2020-07-02 09:20:21 +0900

    axios 를 이용한 rest api 호출 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 //import PropTypes from 'prop-types'
 
 class App extends React.Component {
 
   state = {
-    isLoading: true,
-    movies: []
+    isLoading: true
+  };
+
+  getMovies = () => {
+    const movies = axios.get("https://yts-proxy.now.sh/list_movies.json")
+    //const data = await Response.json();
+    return movies
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false})
-    }, 6000)
+    this.getMovies().then(result => {
+      console.log(result);
+      this.setState({isLoading:false, movies:this.movies}) 
+    }); 
+    console.log("complete")
   }
 
   render() {
